190648cf0e2f6339bec45d588d27a0da5820d3ee
Mary Shiposha <rusya7891@mail.ru>
2020-05-30 02:47:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DP_LevenshteinDistance
+{
+    class Program
+    {/*алгоритм поиска расстояния Левенштейна. 
+        На вход алгоритм принимает две строки, 
+        а возвращает расстояние Левенштейна.
+
+Расстояние Левенштейна — это минимальное количество 
+правок (вставки, удаления или изменения одного символа на другой),
+которое нужно сделать, чтобы из первого слова получить второе.*/
+
+        public static int LevenshteinDistance(string first, string second)
+        {
+            var opt = new int[first.Length + 1, second.Length + 1];
+            for (var i = 0; i <= first.Length; ++i) opt[i, 0] = i;
+            for (var j = 0; j <= second.Length; ++j) opt[0, j] = j;
+            for (var i = 1; i <= first.Length; ++i)
+                for (var j = 1; j <= second.Length; ++j)
+                {
+                    if (first[i - 1] == second[j - 1])
+                        opt[i, j] = opt[i - 1, j - 1];
+                    else
+                        opt[i, j] = 1 + Math.Min(Math.Min(opt[i - 1, j], opt[i, j - 1]), opt[i - 1, j - 1]);
+                }
+            return opt[first.Length, second.Length];
+        }
+        static void Main(string[] args)
+        {
+            Assert.AreEqual(0, LevenshteinDistance("a", "a"));
+            Assert.AreEqual(1, LevenshteinDistance("a", "aa"));
+            Assert.AreEqual(2, LevenshteinDistance("кот", "клон"));
+            Assert.AreEqual(3, LevenshteinDistance("yabx", "abcd"));
+            Assert.AreEqual(4, LevenshteinDistance("студент", "солдат"));
+            SecretTests();
+            Console.WriteLine("OK");
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("DP LevenshteinDistance")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("DP LevenshteinDistance")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("01e8e7e0-ec55-421e-8ae6-43bf23e35754")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
